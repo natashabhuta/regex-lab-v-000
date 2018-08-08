e6df8149cc34d15cfb07e264aa3d07d23010bf74
natashabhuta <natasha.bhuta@gmail.com>
2018-08-08 17:17:03 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,7 +16,7 @@ end
 
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
- if text.match(/^[A-Z]+[\.!?]$/) then
+ if !!text.match(/^[A-Z].+\W$/)
    return true
  else 
    return false
